813c26e41ef7fc09a08629b7198ce9b244f123e5
dhunter1991 <hunterdylan882@gmail.com>
2015-10-11 13:51:38 -0400

    commit 2

--- conditionals assignment/js/script.js
+++ conditionals assignment/js/script.js
@@ -1,35 +1,33 @@
 var temp = prompt("if it cold enough to snow, enter the temperature in fahrenheit");
 var rain = prompt("is it expected to rain?");
 var work = prompt("do you have to work the next day");
-var truck = prompt("do you have a vehicle that can make it through snow");
-var amountOfRain = prompt("how much is the expected rain fall");
+var amountOfRain = prompt("how much is the expected rain fall in inches");
 var amountOfSnow = amountOfRain * 10;
-var snow = temp < 32 + rain;
-var noWork = snow + work;
+var snow = temp <= 32 + rain == "yes";
+
+
 
 if ("") {
-    prompt ("you forgot to input some information \n please enter in a value");
-}else{
-    console.log(snow);
-}
+    prompt("you forgot to input some information \n please enter in a value");
 
-if (temp <= 32) {
+}else if (temp <= 32) {
     console.log ("it is cold enough to snow");
-
-}else if (truck == "yes"){
-
-    console.log ("you have a vehicle that can make it through the snow");
 
 }else {
 
-    console.log ("you do not have a vehicle that can make it in the snow");
+    console.log("it is not cold enough to snow");
+
 }
 
-(temp <= 32 + rain == "yes") ? console.log ("it will snow") : console.log ("it will not snow");
-
-(truck = "yes") ? console.log ("you have to go work") : console.log ("you do not have to go work");
+(rain == "yes") ? console.log ("there is a chance it will snow") : console.log ("it will not snow");
 
 
+if (temp <= 32 && rain == "yes") {
+    console.log("you do not have to go to work");
+}else{
+    console.log("you have to go work");
+}
+console.log("the expected snowfall is " + amountOfSnow + " inches");
 
 
 
