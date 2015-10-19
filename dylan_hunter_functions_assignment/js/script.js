f73fd0c65fafa15d0b7dd6b9f8753b7fd3f95203
dhunter1991 <hunterdylan882@gmail.com>
2015-10-19 19:40:18 -0400

    commit 3

--- dylan_hunter_functions_assignment/js/script.js
+++ dylan_hunter_functions_assignment/js/script.js
@@ -3,11 +3,11 @@
 // 10/19/2015
 
 
-
+// variable prompt for the user to choose which lottery numbers they want to check
 var floridaLottery = prompt("will you be checking the florida lottery yes or no?\n If no you will be checking the Powerball lottery numbers.");
 
 
-
+// function for random numbers generator
 var getRandom = function (min,max) {   // defining
     var number = Math.floor(Math.random() * max) + min;
     return number;
@@ -15,15 +15,9 @@ var getRandom = function (min,max) {   // defining
 // numbers for the florida lottery
 var f = getRandom(1, 53); // invoking
 // console log for the florida lottery numbers
-console.log(f);
-// variable for the power ball main numbers
 var p = getRandom(1,59);
 // console log for the power ball main numbers
-console.log(p);
-// variable for the power ball
 var pB = getRandom(1,35);
-// console log for the power ball
-console.log(pB);
 // variable to stop the while loop
 var a = 1;
 // while loop made for the user input for the florida lottery numbers
